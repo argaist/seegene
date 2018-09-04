fdd074cc7b54e95ac575074a8f69b3d5986d0f37
twbae <twbae@DESKTOP-5RGOGAS>
2018-09-04 17:23:33 +0900

    wfe

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -3,6 +3,6 @@ package hello;
 public class hello {
 	public static void main(String[] agrs){
 		System.out.println("Hello World ~ ");
-		System.out.println("Hello World2 ~ ");
+		//System.out.println("Hello World2 ~ ");
 	}
 }
