a1d4f8e5de275e63376dec161e1e9e1bde114f25
twbae <twbae@DESKTOP-5RGOGAS>
2018-09-06 11:32:23 +0900

    asd

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -4,6 +4,6 @@ public class hello {
 	public static void main(String[] agrs){
 		System.out.println("Hello World ~ ");
 		System.out.println("Hello World2 ~ ");
-		//System.out.println("Hello World3 ~ ");
+		System.out.println("Hello World3 ~ ");
 	}
 }
